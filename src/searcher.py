a7d6bbba17cd7ef8302e35bc9ab6bbb21ce59883
RenanCampista <renan.campista@edu.ufes.br>
2024-10-09 19:10:33 -0300

    feat: add searcher

---
+++ src/searcher.py
@@ -0,0 +1,135 @@
+"""Obtém URLs dos posts"""
+
+from enum import Enum
+import requests
+import os
+import sys
+import pandas as pd
+from dotenv import load_dotenv
+import re
+import time
+from utils.utils import SocialNetwork
+import utils.utils as utils
+import random
+from bs4 import BeautifulSoup
+
+
+class Searcher:
+    CSE_API_URL = "https://www.googleapis.com/customsearch/v1"
+
+    def __init__(self, cse_id: str, num_keys: int):
+        self.cse_id = cse_id
+        self.keys = []
+        
+        for key in range(num_keys):
+            self.keys.append([utils.env_variable(f"CSE_API_KEY_{key}"), 0, True])
+
+    def start_search(self, query: str, social_network: SocialNetwork, max_results: int) -> str:
+        """Searches Google for the query and returns the first relevant URL."""
+        
+        for attempt in range(len(self.keys)):
+            if not self.keys[attempt][2] or self.keys[attempt][1] > 100:  # Se chave estiver desativada or ter atingido 100 requisições, pule para a próxima
+                continue
+            
+            params = {
+                'q': query,
+                'key': self.keys[attempt][0],
+                'cx': self.cse_id,
+                'num': max_results
+            }
+            try:
+                response = requests.get(self.CSE_API_URL, params=params)
+                response.raise_for_status()
+                search_results = response.json()
+                self.keys[attempt][1] += 1
+                
+                items = search_results.get('items', [])
+                for item in items:
+                    link = item['link']
+                    if social_network.is_valid_link(link):
+                        return link
+                return ''
+            
+            except requests.exceptions.HTTPError as e:
+                if response.status_code == 429:
+                    # Acabou a cota da api
+                    self.keys[attempt][2] = False
+                else:
+                    print(f"Erro na requisição: {e}")
+        return ''
+            
+            
+            
+class SearchEngineAlternatives(Enum):
+    """The search engines used to search for the posts."""
+    BING = "https://www.bing.com/search?q="
+    DUCKDUCKGO =  "https://duckduckgo.com/html/?q="
+    
+    def get_url(self) -> str:
+        return self.value
+    
+
+def search_alternative(query: str, social_network: SocialNetwork):
+    USER_AGENTS = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edge/91.0.864.48",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    ]
+    
+    headers = {
+        "User-Agent": random.choice(USER_AGENTS),
+    }
+    
+    search_engines = [(engine.get_url() + query)
+                      for engine in SearchEngineAlternatives]
+
+    for search_url in search_engines:
+        response = requests.get(search_url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        for link in soup.find_all('a', href=True):
+            href = link['href']
+            if social_network.is_valid_link(href):
+                return href
+            return ''
+        time.sleep(random.uniform(1, 3))
+    return ''
+
+
+def main():
+    google_ofc_searcher = Searcher(
+        cse_id=utils.env_variable("CSE_ID"),
+        num_keys=utils.env_variable("NUM_KEYS")
+    )
+    
+    social_network = SocialNetwork.get_social_network()
+    file_name = utils.list_files_and_get_input()
+    utils.validate_file_extension(file_name, '.csv')
+    data_posts = utils.read_posts(file_name)
+    
+    for index, row in data_posts.iterrows():
+        text = row['message']
+        query = utils.filter_bmp_characters(text)
+        
+        post_url = google_ofc_searcher.start_search(
+            query=query,
+            social_network=social_network,
+            max_results=10
+        )
+        
+        if post_url:
+            post_url = utils.extract_relevant_url(post_url)
+            data_posts.at[index, social_network.get_post_url_column()] = post_url
+            print(f"URL encontrada para a linha {index + 1}: {post_url:}")
+        else:
+            new_query = social_network.generate_alternative_query(query)
+            post_url = search_alternative(new_query, social_network)
+            if post_url:
+                post_url = utils.extract_relevant_url(post_url)
+                data_posts.at[index, social_network.get_post_url_column()] = post_url
+                print(f"URL encontrada para a linha {index + 1} de forma alternativa: {post_url:}")     
+            else:
+                print(f"URL não encontrada para a linha {index + 1}")
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,11 +1,157 @@
 """Funções úteis"""
 
+from enum import Enum
 import sys
 import os
 import pandas as pd
 import re
+from urllib.parse import urlparse
 
 MIX_CARACTERES = 100
+
+
+class SocialNetwork(Enum):
+    """Social networks collected by Content Library"""
+    INSTAGRAM = 'https://www.instagram.com/'
+    FACEBOOK = 'https://www.facebook.com/'
+    
+    
+    def __init__(self, url):
+        self.url = url
+        
+    @staticmethod 
+    def get_social_network() -> 'SocialNetwork':
+        print("Escolha a rede social:\n1 - Instagram\n2 - Facebook")
+        option = input()
+        if option == '1':
+            return SocialNetwork.INSTAGRAM
+        elif option == '2':
+            return SocialNetwork.FACEBOOK
+        else:
+            print("Opção inválida.")
+            return SocialNetwork.get_social_network()
+        
+    def get_post_url_column(self) -> str:
+        if self == self.__class__.INSTAGRAM:
+            return 'url'
+        elif self == self.__class__.FACEBOOK:
+            return 'postUrl'
+        else:
+            raise ValueError(f"Rede social inválida {self}.")
+    
+    def is_valid_link(self, link: str) ->bool:
+        if self == self.__class__.INSTAGRAM:
+            if self.url in link and any(substring in link for substring in ["p/", "tv/", "reel/", "video/", "photo/"]):
+                return True
+            return False
+        elif self == self.__class__.FACEBOOK:
+            if self.url in link and any(substring in link for substring in ["posts/", "videos/", "photos/", "groups/"]):
+                return True
+            return False
+        else:
+            raise ValueError(f"Rede social inválida {self}.")
+    
+    def mapping_columns(self) -> list:
+        """Return a list of tuples with the original column name, the new column name and the default value."""
+        if self == self.__class__.INSTAGRAM:
+            return [
+                ('post_owner.name','name', ''),
+                ('creation_time', 'time', ''),
+                ('statistics.like_count', 'likes', ''),
+                ('text', 'message', ''),
+                ('post_owner.id', 'profileId', ''),
+                ('', 'commentId', None), 
+                ('post_owner.username', 'username', ''),
+                ('', 'parentCommentId', None), 
+                ('', 'replies', None),
+                ('', 'reply', False),
+                ('', 'shortcode', ''),
+                ('', 'reaction', None),
+                ('', 'isVideo', ''), #TODO: verificar o media_type 
+                ('statistics.comment_count', 'comments', ''),
+                ('', 'url', ''),
+                ('', 'profileUrl', ''),
+                ('statistics.views', 'videoViewCount', ''),
+                ('', 'isPrivateUser', None),
+                ('', 'isVerifiedUser', None),
+                ('', 'displayUrl', ''),
+                ('', 'followersCount', ''),
+                ('id', 'id', ''),
+                ('', 'caption', None),
+                ('', 'thumbnail', None),
+                ('', 'accessibilityCaption', None),
+                ('', 'commentsDisabled', None),
+                ('', 'videoDuration', ''),
+                ('media_type', 'productType', ''),
+                ('', 'isSponsored', False), 
+                ('', 'locationName', None),
+                ('', 'mediaCount', None),
+                ('multimedia', 'media', ''),
+                ('', 'owner', None),
+                ('', 'profileImage', None),
+                ('hashtags', 'terms', '')    
+            ]
+        elif self == self.__class__.FACEBOOK:
+            return [
+                ('id', 'id', ''),
+                ('post_owner.name','name', ''),
+                ('post_owner.username', 'nickName', ''),
+                ('creation_time', 'time', ''),
+                ('text', 'message', ''),
+                ('post_owner.id', 'profileId', ''),
+                ('', 'postId', ''),
+                ('multimedia', 'media', ''),
+                ('', 'attachments', None),
+                #('', 'countReactionTypes', '') # Não terá esse. Reações separadas abaixo
+                ('statistics.comment_count', 'countComment', ''),
+                ('statistics.views', 'countSeen', ''),
+                ('statistics.share_count', 'countShare', ''),
+                ('statistics.reaction_count', 'countReaction', ''),
+                ('statistics.like_count', 'countLike', ''),
+                ('statistics.love_count', 'countLove', ''),
+                ('statistics.care_count', 'countCare', ''),
+                ('statistics.haha_count', 'countHaha', ''),
+                ('statistics.wow_count', 'countWow', ''),
+                ('statistics.sad_count', 'countSad', ''),
+                ('statistics.angry_count', 'countAngry', ''),
+                ('', 'profileUrl', ''),
+                ('', 'postUrl', ''),
+                ('', 'adUrl', '')
+            ]
+        else:
+            raise ValueError(f"Rede social inválida {self}.")
+        
+    def generate_profile_url(self, username: str) -> str:
+        """Generate the profile URL based on the username."""
+        return f"{self.url}{username}"
+             
+    def generate_alternative_query(self, query: str) ->str:
+        if self == self.__class__.INSTAGRAM:
+            return f'site:+instagram.com+{query}'
+        elif self == self.__class__.FACEBOOK:
+            return f'site:+facebook.com+{query}'
+        else:
+            raise ValueError(f"Rede social inválida {self}.")
+             
+    def fix_df(self, posts_cl: pd.DataFrame) -> pd.DataFrame:
+        """Fix the DataFrame based on the social network."""
+        df_fixed = pd.DataFrame()
+        
+        for cl_column, ec_column, value in self.mapping_columns():
+            df_fixed[ec_column] = posts_cl[cl_column] if cl_column else value
+
+        df_fixed['profileUrl'] = df_fixed['username'].apply(self.generate_profile_url)
+
+        return df_fixed
+
+
+
+def env_variable(var_name: str) -> str:
+    """Gets an environment variable or raises an exception if it is not set."""    
+    if not (var_value := os.getenv(var_name)):
+        raise ValueError(f"Variável de ambiente {var_name} não definida. Verifique o arquivo .env.")
+    return var_value
+
 
 def validate_file_extension(file_name: str, extension: str):
     """Check if the file has the correct extension."""
@@ -33,7 +179,12 @@ def list_files_and_get_input() -> str:
                 return user_input
             else:
                 print("Invalid file name. Please try again.")
-                
+  
+  
+def filter_bmp_characters(text: str) -> str:
+    """Remove characters outside the Basic Multilingual Plane (BMP)."""
+    return re.sub(r'[^\u0000-\uFFFF]', '', text)
+              
                 
 def read_posts(file_path: str) -> pd.DataFrame:
     """Reads the CSV file with the posts and returns a DataFrame."""
@@ -71,3 +222,9 @@ def clean_data_posts(data_posts: pd.DataFrame) -> pd.DataFrame:
     print(f"Posts restantes: {prev_length - duplicates - min_caracteres_removed}.")
     
     return data_posts
+
+
+def extract_relevant_url(url: str) -> str:
+    """Extracts the relevant part of the URL."""
+    parsed_url = urlparse(url)
+    return f"{parsed_url.scheme}://{parsed_url.netloc}{parsed_url.path}"
