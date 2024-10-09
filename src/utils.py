7c157f849044ca8c2b6fbc556165987829d20cc6
RenanCampista <renan.campista@edu.ufes.br>
2024-10-09 13:54:45 -0300

    refactor: cria o módulo utils

---
+++ src/utils.py
@@ -0,0 +1,47 @@
+"""Funções úteis"""
+
+import sys
+import os
+import pandas as pd
+
+def validate_file_extension(file_name: str, extension: str):
+    """Check if the file has the correct extension."""
+    if not file_name.endswith(extension):
+        print(f"Arquivo inválido. O arquivo deve ser um {extension}")
+        sys.exit(1) 
+        
+
+def list_files_and_get_input() -> str:
+    """List files and get user input for file selection."""
+    files = [file for file in os.listdir('.') if os.path.isfile(file) and not file.startswith('.')]
+    while True:
+        user_input = input('Please enter the file name ("?" to list): ')
+        if user_input == '?':
+            for i, file in enumerate(files, 1):
+                print(f"{i}. {file}")
+        elif user_input.isdigit():
+            file_index = int(user_input) - 1
+            if 0 <= file_index < len(files):
+                return files[file_index]
+            else:
+                print("Invalid number. Please try again.")      
+        else:
+            if user_input in files:
+                return user_input
+            else:
+                print("Invalid file name. Please try again.")
+                
+                
+def read_posts(file_path: str) -> pd.DataFrame:
+    """Reads the CSV file with the posts and returns a DataFrame."""
+    try:
+        return pd.read_csv(file_path)
+    except FileNotFoundError:
+        print(f"Arquivo não encontrado: {file_path}")
+        sys.exit(1)
+    except pd.errors.ParserError:
+        print(f"Erro ao ler o arquivo: {file_path}")
+        sys.exit(1)
+    except KeyError as e:
+        print(f"Coluna não encontrada: {e}")
+        sys.exit(1)
